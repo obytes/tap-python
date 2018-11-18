c00a8ac156f79cb2069905132a6b659c4d0f3b7b
aitbrahim <aitbrahim.mostapha@gmail.com>
2018-11-18 15:53:08 +0100

    fix abstract api resources

--- tap/api_resources/abstract/listeable_api_resource.py
+++ tap/api_resources/abstract/listeable_api_resource.py
@@ -1,6 +1,7 @@
-from tap.api_resources.abstract import APIResource
-from tap import util
-from tap.api_requestor import APIRequestor
+from __future__ import absolute_import, division, print_function
+
+from tap import api_requestor, util
+from tap.api_resources.abstract.api_resource import APIResource
 
 
 class ListeableAPIResource(APIResource):
@@ -10,16 +11,13 @@ class ListeableAPIResource(APIResource):
         return cls.list(*args, **params).auto_paging_iter()
 
     @classmethod
-    def list(cls, api_key=None, stripe_version=None, stripe_account=None,
-             **params):
-        requestor = APIRequestor(api_key,
-                                 api_base=cls.api_base(),
-                                 api_version=stripe_version,
-                                 account=stripe_account)
+    def list(cls, api_key=None, tap_version=None, tap_account=None, **params):
+
+        requestor = api_requestor.APIRequestor(api_key, api_version=tap_version,
+                                               account=tap_account)
         url = cls.class_url()
         response, api_key = requestor.request('get', url, params)
-        stripe_object = util.convert_to_stripe_object(response, api_key,
-                                                      stripe_version,
-                                                      stripe_account)
-        stripe_object._retrieve_params = params
-        return stripe_object
+        tap_object = util.convert_to_tap_object(response, api_key, tap_version,
+                                                   tap_account)
+
+        return tap_object
--- tap/api_resources/abstract/updateable_api_resource.py
+++ tap/api_resources/abstract/updateable_api_resource.py
@@ -1,22 +1,21 @@
-from tap.api_resources.abstract import APIResource
-from tap import util
-from tap.api_requestor import APIRequestor
+from __future__ import absolute_import, division, print_function
+
+from tap import api_requestor, util
+from tap.api_resources.abstract.api_resource import APIResource
 from urllib.parse import quote_plus
 
 
 class UpdateableAPIResource(APIResource):
 
     @classmethod
-    def _modify(cls, url, api_key=None, idempotency_key=None,
-                stripe_version=None, stripe_account=None, **params):
-        requestor = APIRequestor(api_key,
-                                 api_version=stripe_version,
-                                 account=stripe_account)
+    def _modify(cls, url, api_key=None, stripe_version=None,
+                stripe_account=None, **params):
 
-        headers = util.populate_headers(idempotency_key)
-        response, api_key = requestor.request('post', url, params, headers)
-        return util.convert_to_stripe_object(response, api_key, stripe_version,
-                                             stripe_account)
+        requestor = api_requestor.APIRequestor(api_key, api_version=stripe_version,
+                                               account=stripe_account)
+
+        response, api_key = requestor.request('post', url, params)
+        return util.convert_to_tap_object(response, api_key, stripe_version, stripe_account)
 
     @classmethod
     def modify(cls, sid, **params):
