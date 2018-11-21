ef2d75ce33b2eba864e38095b8ceb055690a442c
aitbrahim <aitbrahim.mostapha@gmail.com>
2018-11-21 01:43:06 +0100

    update, list charge

--- tap/api_resources/charge.py
+++ tap/api_resources/charge.py
@@ -1,6 +1,10 @@
+from abstract import ListeableAPIResource
+from abstract import UpdateableAPIResource
 from abstract import CreateableAPIResource
 
 
-class Charge(CreateableAPIResource):
+class Charge(CreateableAPIResource,
+             UpdateableAPIResource,
+             ListeableAPIResource):
 
     OBJECT_NAME = 'charge'
